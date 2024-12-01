22a92767ae8ce1cc2271be989617cfe7a5f4c67c
jhws42 <jann.wissbach@mni.thm.de>
2024-12-01 21:43:28 +0100

    added conversions

--- src/date_conversion.rs
+++ src/date_conversion.rs
@@ -1,5 +1,12 @@
 use crate::structs::date::Date;
+use std::time::{Duration, SystemTime};
+use chrono::{DateTime, Local, NaiveDateTime, Utc};
 
-pub fn convert_time_to_actual_date(date: Date) {
-    println!("{:?}", date);
+pub fn convert_time_to_actual_date(time: u64) {
+    let now: DateTime<Local> = Local::now();
+    let seconds: u32 = (time % (60)) as u32;
+    let minutes: u32 = ((time/60) % (60)) as u32;
+    let hours: u32 = ((time/(60*60)) % 24) as u32; 
+    let days: u32 = ((time/(60*24*60))) as u32; 
+    println!("{}-{}:{}:{}", days, hours, minutes, seconds);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use structs::date::Date;
 
 fn main() {
     let day: Date = Date::new(123142, String::from("Essen"));
-    convert_time_to_actual_date(day);
+    convert_time_to_actual_date(930001);
 }
-
 
